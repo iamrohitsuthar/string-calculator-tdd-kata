a8d521c149091c97e8710cd2a55b8a0d9fb51a5a
iamrohitsuthar <iamrohitsuthar@gmail.com>
2021-07-16 18:06:34 +0530

    Refactor string_calculator code

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,50 +1,48 @@
-import 'package:string_calculator_tdd_kata/negative_number_exception.dart';
+import 'negative_number_exception.dart';
 
 class StringCalculator {
   int add(String delimitersAndNumbers) {
-    if (delimitersAndNumbers.isEmpty)
-      return 0;
-    else
-      return getNumbersSum(delimitersAndNumbers);
+    if (delimitersAndNumbers.isEmpty) return 0;
+    return getNumbersSum(delimitersAndNumbers);
   }
 
   int getNumbersSum(String delimitersAndNumbers) {
-    int sum = 0;
-    List<String> extractedNumbers =
-        getNumbersSplitByDelimeter(delimitersAndNumbers);
-    StringBuffer negativeNumbers = StringBuffer();
+    var sum = 0;
+    var extractedNumbers = getNumbersSplitByDelimeter(delimitersAndNumbers);
+    var negativeNumbers = StringBuffer();
     extractedNumbers.forEach((number) {
-      if (int.parse(number) < 0)
+      if (int.parse(number) < 0) {
         negativeNumbers.write(number + ' ');
-      else {
+      } else {
         if (int.parse(number) <= 1000) sum += int.parse(number);
       }
     });
-    if (negativeNumbers.isNotEmpty)
+    if (negativeNumbers.isNotEmpty) {
       throw NegativeNumberException(
           'negatives not allowed ${negativeNumbers.toString().trim()}');
+    }
     return sum;
   }
 
   List<String> getNumbersSplitByDelimeter(String delimitersAndNumbers) {
-    if (delimitersAndNumbers.startsWith("//")) {
+    if (delimitersAndNumbers.startsWith('//')) {
       return getNumbersSplitByCustomDelimiter(delimitersAndNumbers);
     } else {
       // split numbers with delimeter as , or \n or ;
-      return delimitersAndNumbers.split(RegExp("[,\n;]"));
+      return delimitersAndNumbers.split(RegExp('[,\n;]'));
     }
   }
 
   List<String> getNumbersSplitByCustomDelimiter(String delimitersAndNumbers) {
-    int delimitersEnd = delimitersAndNumbers.lastIndexOf('\n');
-    String rawDelimiters = delimitersAndNumbers.substring(0, delimitersEnd);
-    String rawNumbers = delimitersAndNumbers.substring(delimitersEnd + 1);
-    String customDelimiters = getCustomDelimiters(rawDelimiters);
+    var delimitersEnd = delimitersAndNumbers.lastIndexOf('\n');
+    var rawDelimiters = delimitersAndNumbers.substring(0, delimitersEnd);
+    var rawNumbers = delimitersAndNumbers.substring(delimitersEnd + 1);
+    var customDelimiters = getCustomDelimiters(rawDelimiters);
     return rawNumbers.split(RegExp("($customDelimiters)"));
   }
 
   String getCustomDelimiters(String rawDelimiters) {
-    String bracketsExtractedDelimiters =
+    var bracketsExtractedDelimiters =
         getDelimitersEnclosedWithinBrackets(rawDelimiters);
     if (bracketsExtractedDelimiters.isEmpty) {
       // if there is no delimiter with [delimeter] format in the custom delimiter string
@@ -55,9 +53,9 @@ class StringCalculator {
   }
 
   String getDelimitersEnclosedWithinBrackets(String rawDelimiters) {
-    StringBuffer extractedDelimiters = StringBuffer();
-    int start = 0;
-    for (int index = 0; index < rawDelimiters.length; index++) {
+    var extractedDelimiters = StringBuffer();
+    var start = 0;
+    for (var index = 0; index < rawDelimiters.length; index++) {
       var symbol = rawDelimiters[index];
       if (symbol == '[')
         start = index;
